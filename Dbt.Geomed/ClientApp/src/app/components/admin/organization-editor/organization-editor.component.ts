ceb7d6d3b75ea7abee2638fe8e8727f8873358a7
Владимир Цыганков <tsygankov@skytecs.ru>
2019-06-08 15:10:33 +0300

    adds pages

---
+++ Dbt.Geomed/ClientApp/src/app/components/admin/organization-editor/organization-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-editor',
+  templateUrl: './organization-editor.component.html',
+  styleUrls: ['./organization-editor.component.css']
+})
+export class OrganizationEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Dbt.Geomed/ClientApp/src/app/components/admin/service-editor/service-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-editor',
+  templateUrl: './service-editor.component.html',
+  styleUrls: ['./service-editor.component.css']
+})
+export class ServiceEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
