f55ea483b8ca6e4f65b0589b5f8ceabb8358e48b
klogo-hopper <sandro-248@mail.ru>
2019-06-08 22:00:52 +0300

    adds categories

---
+++ Dbt.Geomed/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dbt.Geomed.Models
+{
+    public class Category : Entity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
