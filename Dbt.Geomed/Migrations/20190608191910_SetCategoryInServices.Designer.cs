7ecfba93378bbc66d963b35cc3e399aeae59104e
klogo-hopper <sandro-248@mail.ru>
2019-06-08 22:57:56 +0300

    adds api categories

--- Dbt.Geomed/Migrations/20190608191910_SetCategoryInServices.Designer.cs
+++ Dbt.Geomed/Migrations/20190608191910_SetCategoryInServices.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Dbt.Geomed.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Dbt.Geomed.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190608191910_SetCategoryInServices")]
+    partial class SetCategoryInServices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
