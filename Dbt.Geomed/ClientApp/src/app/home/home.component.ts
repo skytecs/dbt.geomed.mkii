2bbfd444db6c9922e4e4de59ea7e36e0da8d65d9
Владимир Цыганков <tsygankov@skytecs.ru>
2019-06-08 22:11:29 +0300

    adds main page

--- Dbt.Geomed/ClientApp/src/app/home/home.component.ts
+++ Dbt.Geomed/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Category } from './models/category';
+import { Service } from './models/service';
 
 @Component({
   selector: 'app-home',
@@ -6,16 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent {
-  private _step: number;
+  private _categories: Array<Category>;
+
+  private _selectedServices: Array<number>;
 
   public constructor() {
-    this._step = 0;
+    this._selectedServices = [];
+
+    const category1 = new Category(1, "Узи");
+    const category2 = new Category(2, "Кардиологогия");
+
+    category1.services = [
+      new Service(1, "Узи почек", 1),
+      new Service(2, "Узи почек", 1),
+      new Service(3, "Узи почек", 1)
+    ];
+
+    category2.services = [
+      new Service(4, "Первичная консультация кардиолога", 2),
+      new Service(5, "Повторная консультация кардиолога", 2),
+    ];
+
+    this._categories = [
+      category1,
+      category2
+    ];
   }
 
-  public get step(): number { return this._step; }
+  public get categories(): Array<Category> { return this._categories; }
 
-  public next = (): void => {
-    this._step++;
+  public checked = (item: Service): boolean => {
+    return this._selectedServices.some((x: number): boolean => x === item.id);
+  }
+
+  public check = (item: Service): void => {
+    let index: number = this._selectedServices.findIndex((x: number) => x === item.id);
+
+    if (index === -1) {
+      this._selectedServices.push(item.id);
+    } else {
+      this._selectedServices.splice(index, 1);
+    }
+  }
+
+  public clear = (): void => {
+    this._selectedServices = [];
   }
 
 }
