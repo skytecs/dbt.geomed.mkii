3b16995ff34d06066bc1e2368d0ed8c23eaadf15
Владимир Цыганков <tsygankov@skytecs.ru>
2019-06-08 15:41:39 +0300

    adds organizations model and service

---
+++ Dbt.Geomed/ClientApp/src/app/components/admin/organizations/models/organization.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Organization = /** @class */ (function () {
+    function Organization(id) {
+        this._id = id;
+    }
+    Object.defineProperty(Organization.prototype, "id", {
+        get: function () { return this._id; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Organization.prototype, "name", {
+        get: function () { return this._name; },
+        set: function (value) { this._name = value; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Organization.prototype, "lat", {
+        get: function () { return this._lat; },
+        set: function (value) { this._lat = value; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Organization.prototype, "lon", {
+        get: function () { return this._lon; },
+        set: function (value) { this._lon = value; },
+        enumerable: true,
+        configurable: true
+    });
+    return Organization;
+}());
+exports.Organization = Organization;
+//# sourceMappingURL=organization.js.map
--- Dbt.Geomed/ClientApp/src/app/components/admin/organizations/organizations.component.ts
+++ Dbt.Geomed/ClientApp/src/app/components/admin/organizations/organizations.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { OrganizationsService } from './services/organizations.service';
+import { Organization } from './models/organization';
 
 @Component({
   selector: 'app-organizations',
   templateUrl: './organizations.component.html',
-  styleUrls: ['./organizations.component.scss']
+  styleUrls: ['./organizations.component.scss'],
+  providers: [OrganizationsService]
 })
 export class OrganizationsComponent implements OnInit {
+  private _organizationsService: OrganizationsService;
 
-  constructor() { }
+  private _collection: Array<Organization>;
 
-  ngOnInit() {
+  constructor(organizationsService: OrganizationsService) {
+    this._organizationsService = organizationsService;
   }
 
+  ngOnInit() {
+    this._organizationsService.load().subscribe((result: Array<Organization>): void => {
+      this._collection = result;
+    });
+  }
+
+  public get collection(): Array<Organization> { return this._collection; }
 }
