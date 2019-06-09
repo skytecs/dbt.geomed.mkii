48071747ceee39b2b65ba0e949c47d01db1c1c1b
dmitry nadolin <nadolin@skytecs.ru>
2019-06-09 09:04:36 +0300

    fixes migrations

--- Dbt.Geomed/Migrations/20190609060114_AddsNhi.cs
+++ Dbt.Geomed/Migrations/20190609060114_AddsNhi.cs
@@ -10,43 +10,13 @@ namespace Dbt.Geomed.Migrations
                 name: "IsNhi",
                 table: "Prices",
                 nullable: true);
-
-            migrationBuilder.AddColumn<long>(
-                name: "UserId",
-                table: "Companies",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Companies_UserId",
-                table: "Companies",
-                column: "UserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Companies_Users_UserId",
-                table: "Companies",
-                column: "UserId",
-                principalTable: "Users",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Restrict);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Companies_Users_UserId",
-                table: "Companies");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Companies_UserId",
-                table: "Companies");
-
             migrationBuilder.DropColumn(
                 name: "IsNhi",
                 table: "Prices");
-
-            migrationBuilder.DropColumn(
-                name: "UserId",
-                table: "Companies");
         }
     }
 }
