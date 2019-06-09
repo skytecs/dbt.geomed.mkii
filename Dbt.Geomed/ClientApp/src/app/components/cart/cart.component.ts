46ef27907f8ac7656d1c4efcda8ff0cc79a375c3
Владимир Цыганков <tsygankov@skytecs.ru>
2019-06-09 14:02:38 +0300

    merge

--- Dbt.Geomed/ClientApp/src/app/components/cart/cart.component.ts
+++ Dbt.Geomed/ClientApp/src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute} from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
@@ -7,12 +8,18 @@ import {Router, ActivatedRoute} from "@angular/router";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute)  {  }
+  private _location: Location;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   private _priceIds: Array<number> = [];
 
   ngOnInit() {
     this._priceIds = this.activatedRoute.snapshot.queryParamMap.getAll("service").map(value => parseInt(value));
+  }
+
+  public goBack = (): void => {
+    this._location.back();
   }
 
 }
